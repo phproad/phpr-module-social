6339e1f5f00dd1c5aa451affc60eb84b6fcaa643
Sam Georges <sam@daftspunk.com>
2012-09-29 21:12:23 +1000

    Add like button

--- classes/social_facebook.php
+++ classes/social_facebook.php
@@ -18,8 +18,11 @@ class Social_Facebook
         return new Social_Facebook($options);
     }
 
-    public function facepile($options = array()) 
+    public function facepile($url=null, $options = array()) 
     {
+        if (!$url)
+            root_url('/', true);
+
         extract(array_merge(array(
             'size' => 'medium',
             'colorscheme' => 'light',
@@ -29,7 +32,7 @@ class Social_Facebook
         
 
         $str = '<iframe src="//www.facebook.com/plugins/facepile.php?'
-            .'href='.urlencode(root_url('/', true))
+            .'href='.urlencode($url)
             .'&amp;action&amp;size='.$size
             .'&amp;max_rows='.$max_rows
             .'&amp;width='.$width
@@ -38,6 +41,36 @@ class Social_Facebook
             .'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$width.'px;" allowTransparency="true"></iframe>';
 
         return $str;
+    }
+
+    public function like_button($url, $options = array())
+    {
+        if (!$url)
+            root_url('/', true);
+
+        extract(array_merge(array(
+            'show_send_button' => false,
+            'show_faces' => false,
+            'action' => 'like', // like or recommend
+            'size' => 'medium',
+            'colorscheme' => 'light',
+            'max_rows' => 1,
+            'width' => 100,
+            'height' => 35
+        ), $this->options, $options));
+
+        $str = '<iframe src="//www.facebook.com/plugins/like.php'
+            .'href='.urlencode($url)
+            .'&amp;send='.(($show_send_button) ? 'true' : 'false')
+            .'&amp;show_faces='.(($show_faces) ? 'true' : 'false')
+            .'&amp;action='.$action
+            .'&amp;colorscheme='.$colorscheme
+            .'&amp;font'
+            .'&amp;height='.$height
+            .'&amp;appId='.$this->options['app_id']
+            .'&amp;width='.$width
+            .'&amp;appId='.$this->options['app_id']
+            .'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$width.'px;  height:'.$height.'px;" allowTransparency="true"></iframe>';
     }
 }
 
